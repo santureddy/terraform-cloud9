62e06c4bf309c71b122aec7ab023482cf60ccda9
Santosh Kunadi <santukunadi@gmail.com>
2021-10-16 17:36:38 +0530

    Lecture 57 Fix module name

--- main.tf
+++ main.tf
@@ -18,11 +18,12 @@ resource "random_string" "random" {
 module "container" {
   source      = "./container"
   count       = local.container_count
-  image_in    = module.image.image_out
+  image_in    = module.nodered_image.image_out
   name_in     = join("-", ["nodered", random_string.random[count.index].result])
   int_port_in = 1880
   #external = 1880
   ext_port_in       = var.ext_port[terraform.workspace][count.index]
   container_path_in = "/data"
+
 }
 
